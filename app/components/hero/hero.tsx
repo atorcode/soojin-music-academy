720f203faa8bc2b8b429f5371105120fec818221
atorcode <atorcode@gmail.com>
2023-08-02 20:16:30 -0700

    Add scroll/load transition to hero text section

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // styles
 import styles from "./hero.module.scss";
 
@@ -5,7 +7,24 @@ import styles from "./hero.module.scss";
 import Image from "next/image";
 import { Button } from "../button";
 
+// hooks
+import { useEffect, useRef, useState } from "react";
+
 export const Hero = () => {
+  const [hasTransitionBegun, setHasTransitionBegun] = useState<boolean>(false);
+  const headingRef = useRef<HTMLHeadingElement | null>(null);
+  useEffect(() => {
+    let observer = new IntersectionObserver((entries) =>
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setHasTransitionBegun(true);
+        }
+      })
+    );
+
+    headingRef.current && observer.observe(headingRef.current);
+  }, []);
+
   return (
     <section className={styles["hero"]}>
       <Image
@@ -15,9 +34,29 @@ export const Hero = () => {
         unoptimized
         className={styles["image"]}
       />
-      <div className={styles["text"]}>
-        <h1 className={styles["heading"]}>Discover your voice with us</h1>
-        <Button text="GET STARTED" />
+      <div className={styles["text-section"]}>
+        <h1
+          className={`${styles["heading"]} ${
+            hasTransitionBegun && styles["heading-active"]
+          }`}
+          ref={headingRef}
+        >
+          Discover your voice{" "}
+          <span
+            className={`${styles["delayed-text"]} ${
+              hasTransitionBegun && styles["delayed-text-active"]
+            }`}
+          >
+            with us
+          </span>
+        </h1>
+        <div
+          className={`${styles["button"]} ${
+            hasTransitionBegun && styles["button-active"]
+          }`}
+        >
+          <Button text="GET STARTED" />
+        </div>
       </div>
     </section>
   );
--- app/sections/about-me-section/about-me-section.tsx
+++ app/sections/about-me-section/about-me-section.tsx
@@ -8,7 +8,7 @@ export const AboutMeSection = () => {
   return (
     <section className={styles["section"]}>
       <div className={styles["text"]}>
-        <h2>Hi, I'm Soojin!</h2>
+        <h2>Hi, I&apos;m Soojin!</h2>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
           cumque nam laboriosam temporibus. Cupiditate provident deleniti est
@@ -19,12 +19,6 @@ export const AboutMeSection = () => {
         </p>
       </div>
       <div className={styles["image-container"]}>
-        {/* <Image
-          src="/p1.jpg"
-          alt="Soojin posing with her hands in the air."
-          layout="fill"
-          className={styles["image"]}
-        /> */}
         <Image src="/p2.jpg" alt="" fill className={styles["image"]} />
       </div>
     </section>
