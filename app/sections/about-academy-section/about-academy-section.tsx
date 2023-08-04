d759f3f5e1ed4e081c6b8cc1d39c6d1e0d09be04
atorcode <atorcode@gmail.com>
2023-08-03 17:30:03 -0700

    fill out content for about academy section

--- app/sections/about-academy-section/about-academy-section.tsx
+++ app/sections/about-academy-section/about-academy-section.tsx
@@ -21,9 +21,8 @@ export const AboutAcademySection = () => {
                 className={styles["ribbon"]}
               />
             </div>
-            <p className={styles["ribbon-text"]}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe,
-              rem.
+            <p className={styles["ribbon-caption"]}>
+              Proudly serving the community for two decades.
             </p>
           </section>
         </div>
@@ -31,18 +30,18 @@ export const AboutAcademySection = () => {
           Connecting Through the Language of Music
         </h2>
         <p className={styles["text"]}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, eos
-          aliquam aspernatur eaque sunt eligendi repellendus beatae quisquam,
-          tenetur eum fugit fuga! Natus itaque pariatur at, assumenda nam
-          explicabo officia exercitationem quia reiciendis facere eligendi quo
-          ad tempora porro, sapiente aperiam quod, velit eum magni possimus
-          earum quisquam adipisci minus quam. Reiciendis vero totam soluta
-          tenetur dolorem, quos quidem, porro sit, est deserunt earum laboriosam
-          distinctio. Nulla eveniet, molestiae consequuntur minus temporibus,
-          cum illum repellat pariatur optio ipsam quo omnis asperiores ullam
-          aspernatur perferendis. Dolores nesciunt delectus unde mollitia neque
-          vel repellat magnam atque, cum saepe nemo quidem, laudantium
-          blanditiis.
+          Soojin Music Academy, founded by Maestro Soojin Lee in 2003 as a
+          passion project, has since flourished into an esteemed and cherished
+          establishment within the Federal Way community. Our mission is to
+          ignite a passion for singing and empower students on their individual
+          journeys toward vocal mastery. Moreover, SJM has an exceptional track
+          record of spreading positivity in the community by participating in
+          charity events and building meaningful connections. Our ultimate goal
+          is to create a welcoming space where people gather in mutual
+          appreciation of singing in order to foster a vibrant and harmonious
+          atmosphere. By doing so, we advance our aim of enriching lives through
+          musical expression one day at a time. Join us in celebrating the
+          transformative power of music!
         </p>
       </div>
     </section>
