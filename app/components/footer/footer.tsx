3f79eead97e14b723a97df94bdda224f8e54edbc
atorcode <atorcode@gmail.com>
2023-08-11 17:09:39 -0700

    Implement responsive footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -5,6 +5,7 @@ import styles from "./footer.module.scss";
 
 // icons
 import { PiCaretUpBold } from "react-icons/pi";
+import { GiMusicalScore } from "react-icons/gi";
 
 // components
 import { Link } from "react-scroll";
@@ -22,31 +23,42 @@ export const Footer = () => {
         <PiCaretUpBold className={styles["icon"]} />
       </Link>
       <div className={styles["categories"]}>
-        <div>
-          <h3 className={styles["category"]}>For Business Inquiries</h3>
-          <p>soojin58@hotmail.com</p>
-          <p>(253) 335-7533</p>
+        <div className={styles["category-section"]}>
+          <div>
+            <h3 className={styles["category"]}>For business inquiries</h3>
+            <p>soojin58@hotmail.com</p>
+            <p>(253) 335-7533</p>
+          </div>
+          <div>
+            <p className={`${styles["emphasized"]} ${styles["copyright"]}`}>
+              &copy; 2023 Soojin Music Academy. All rights reserved.
+            </p>
+          </div>
         </div>
-        <div>
-          <h3 className={styles["category"]}>Like our website?</h3>
-          <p>atorcode@gmail.com</p>
-          <p>(253) 335-6420</p>
+        <div className={styles["business"]}>
+          <GiMusicalScore className={styles["logo"]} />
+          <h2>SMA</h2>
         </div>
-      </div>
-      <div>
-        <p className={styles["copyright"]}>
-          &copy; 2023 Soojin Music Academy. All rights reserved.
-        </p>
-        <p>
-          Made with &#x1F496; by{" "}
-          <a
-            href="https://www.linkedin.com/in/alberto-torrigiotti/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            atorcode
-          </a>
-        </p>
+        <div className={styles["category-section"]}>
+          <div>
+            <h3 className={styles["category"]}>Like our website?</h3>
+            <p>atorcode@gmail.com</p>
+            <p>(253) 335-6420</p>
+          </div>
+          <div>
+            <p>
+              Made with &#x1F496; by{" "}
+              <a
+                href="https://www.linkedin.com/in/alberto-torrigiotti/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles["emphasized"]}
+              >
+                atorcode
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   );
